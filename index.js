a807f5c9388b11bf978bb5093452a4801c76e6a6
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-11-27 15:08:38 +0600

    mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +11,20 @@ app.use(express.json());
 app.get('/', (req, res)=> {
     res.send("B sarees server is running on port 5000");
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fxdma7b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function saree(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+saree().catch(err => console.error(err));
 
 app.listen(port, ()=>{
     console.log(`B sarees server is running on port ${port}`)
