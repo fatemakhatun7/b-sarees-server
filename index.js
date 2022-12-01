8c555af95eafd41b35ed96161f1d29995e3ed51b
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-12-01 12:18:21 +0600

    get-user-data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -35,6 +35,16 @@ async function saree(){
             res.send(result);
         })
 
+        app.get('/users', async (req, res) => {
+            let query = {}
+            if(req.query.role){
+                query.role = req.query.role
+            }
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
         app.post('/addProducts', async (req, res) => {
             const addProduct = req.body;
             const result = await addProductCollection.insertOne(addProduct)
@@ -49,6 +59,18 @@ async function saree(){
             const cursor = addProductCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        app.get('/addProducts/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                // console.log(id)
+                const query = { _id: ObjectId(id) }
+                const product = await addProductCollection.findOne(query);
+                res.send(product);
+            } catch (error) {
+                console.log(error);
+            }
         });
 
     }
